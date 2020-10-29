6888493257b836d0cbfa9e148fe82308a86d0695
OhManLolLol <32421828+OhManLolLol@users.noreply.github.com>
2020-10-29 17:51:00 -0400

    buffed layer
    cost is too easy..

--- js/layers.js
+++ js/layers.js
@@ -113,7 +113,7 @@ addLayer("i", {
                                             // Also the amount required to unlock the layer.
     
     type: "normal",                         // Determines the formula used for calculating prestige currency.
-    exponent: 0.5,                          // "normal" prestige gain is (currency^exponent)
+    exponent: 2,                          // "normal" prestige gain is (currency^exponent)
 
     gainMult() {                            // Returns your multiplier to your gain of the prestige resource
         return new Decimal(1)               // Factor in any bonuses multiplying gain here
