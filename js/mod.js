fe3f0444b0ad36ce128aca9bfd3c1bce7b93d551
OhManLolLol <32421828+OhManLolLol@users.noreply.github.com>
2020-10-29 13:49:48 -0400

    Bruh
    Moment

--- js/mod.js
+++ js/mod.js
@@ -12,8 +12,8 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.1",
-	name: "Multiple fixes to a single upgrade. bruh",
+	num: "Alpha 0.3",
+	name: "LOL",
 }
 
 // If you add new functions anywhere inside of a layer, and those functions have an effect when called, add them here.
